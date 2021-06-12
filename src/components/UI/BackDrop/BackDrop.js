7bb745aaf836f50d7759f7bab15ca5ffd68b81fd
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-06-12 11:58:07 -0700

    Fixed backdrop.

--- src/components/UI/BackDrop/BackDrop.js
+++ src/components/UI/BackDrop/BackDrop.js
@@ -1,7 +1,7 @@
 import classes from './BackDrop.module.css';
 
 const BackDrop = (props) => (
-  props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
+  <div className={classes.Backdrop} onClick={props.clicked}></div>
 );
 
 export default BackDrop;
